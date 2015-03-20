e75e9c60556dd8d39329abadaba9167f2d106fd2
Grace <gracelauren909@gmail.com>
2015-03-20 15:22:38 -0700

    capy testing work on date selction with macros possibly

--- spec/features/interacting_with_blog_spec.rb
+++ spec/features/interacting_with_blog_spec.rb
@@ -6,7 +6,7 @@ describe "user interacts with blog" do
     click_link("Create New Post")
     expects_a_page_to_have_content("Post")
 
-    fill_in_forms('Test Post Title', 'Test Post Entry', '2015-03-20 21:53:00 UTC')
+    fill_in_forms('Test Post Title', 'Test Post Entry', '2015/03/20')
     click_on "Create Post"
     expects_a_page_to_have_content("Post successfully added!")
     click_link 'The Gouda Life'
@@ -20,7 +20,7 @@ describe "user interacts with blog" do
     expects_a_page_to_have_content('Test Post Entry')
 
     click_link 'Edit'
-    fill_in_forms('New Post Title', 'New Post Entry', '2015-03-20 21:53:00 UTC')
+    fill_in_forms('New Post Title', 'New Post Entry', '2015/03/20')
     click_on 'Update Post'
     expects_a_page_to_have_content('New Post Title')
     expects_a_page_to_have_content('New Post Entry')
@@ -39,7 +39,7 @@ describe "user interacts with blog" do
     create_a_new_post
     click_link 'Test Post Title'
     click_link "Add a Comment"
-    fill_in_forms('Test Comment Title', 'Test Comment Entry', '2015-03-20 21:53:00 UTC')
+    fill_in_forms('Test Comment Title', 'Test Comment Entry', '2015/03/20')
     expects_a_page_to_have_content("Test Comment Title")
   end
 
@@ -48,9 +48,9 @@ describe "user interacts with blog" do
     create_a_new_post
     click_link 'Test Post Title'
     click_link "Add a Comment"
-    fill_in_forms('Test Comment Title', 'Test Comment Entry', '2015-03-20 21:53:00 UTC')
+    fill_in_forms('Test Comment Title', 'Test Comment Entry', '2015/03/20')
     click_link "Edit"
-    fill_in_forms('New Comment Title', 'New Comment Entry', '2015-03-20 21:53:00 UTC')
+    fill_in_forms('New Comment Title', 'New Comment Entry', '2015/03/20')
     expects_a_page_to_have_content("New Comment Title")
 
     click_link "Delete"
@@ -62,7 +62,7 @@ end
 def create_a_new_post
   click_link("Create New Post")
   expects_a_page_to_have_content("Post")
-  fill_in_forms('Test Post Title', 'Test Post Entry', '2015-03-20 21:53:00 UTC')
+  fill_in_forms('Test Post Title', 'Test Post Entry', '2015/03/20')
   click_on "Create Post"
 end
 
@@ -70,7 +70,7 @@ end
 def fill_in_forms(title, entry, date)
   fill_in('Title', :with => title)
   fill_in('Entry', :with => entry)
-  fill_in('Date', :with => date)
+  select('Date', :from => date)
 end
 
 def expects_a_page_to_have_content(content)

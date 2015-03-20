29dfbe60a3fcc8841d187d05522f8d5cbe926773
Grace <gracelauren909@gmail.com>
2015-03-20 14:19:23 -0700

    add views, routes, and controllers

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,45 @@
+class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment successfully added!"
+      redirect_to post_path(@comment.post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(params[:comment])
+      flash[:notice] = "Comment successfully updated!"
+      redirect_to post_path(@comment.post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      flash[:alert] = "Comment successfully deleted!"
+    end
+    redirect_to post_path(@comment.post)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:title, :entry)
+  end
+end
